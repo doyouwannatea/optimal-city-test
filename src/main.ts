b4992d68dc0e55b913c4c133d422dbab347cfb51
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2023-06-24 15:34:01 +0800

    🆕 сверстал с помощью vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import './styles/main.css'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 
+import { createVuetify } from 'vuetify'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -8,4 +10,5 @@ import App from './App.vue'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(createVuetify({ theme: { defaultTheme: 'dark' } }))
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,47 @@
 <template>
   <main>
-    <nav>
-      <ul>
-        <li v-for="nameValueItem in nameValueStore.nameValueList" :key="nameValueItem.name">
-          <button @click="onSelectElement(nameValueItem)">
-            {{ nameValueItem.name }} - {{ nameValueItem.value }}
-          </button>
-        </li>
-      </ul>
-    </nav>
-    <section>
-      <form @submit.prevent="nameValueStore.editElement(newItemValue)">
-        <label>
-          {{ nameValueStore.currentEditableElement?.name }}
-          <input
-            ref="inputRef"
-            type="text"
-            placeholder="Введите новое значение для элемента"
-            v-model="newItemValue"
-          />
-        </label>
-        <button>Сохранить</button>
-      </form>
-    </section>
+    <VContainer>
+      <VRow>
+        <VCol cols="3">
+          <nav>
+            <VList>
+              <VListItem
+                v-for="nameValueItem in nameValueStore.nameValueList"
+                :key="nameValueItem.name"
+                :title="nameValueItem.name"
+                :subtitle="nameValueItem.value"
+                @click="onSelectElement(nameValueItem)"
+              >
+                <template #prepend>
+                  <div class="mr-3">{ }</div>
+                </template>
+              </VListItem>
+            </VList>
+          </nav>
+        </VCol>
+        <VCol>
+          <section>
+            <VAlert
+              v-if="!nameValueStore.currentEditableElement"
+              icon="mdi-information"
+              text="Выберите элемент"
+            />
+            <form v-else @submit.prevent="nameValueStore.editElement(newItemValue)">
+              <label>
+                {{ nameValueStore.currentEditableElement?.name }}
+                <VTextField
+                  ref="inputRef"
+                  type="text"
+                  placeholder="Введите новое значение для элемента"
+                  v-model="newItemValue"
+                />
+              </label>
+              <VBtn type="submit">Сохранить</VBtn>
+            </form>
+          </section>
+        </VCol>
+      </VRow>
+    </VContainer>
   </main>
 </template>
 
